ce0878928ccb7a9709ec609989617de362531082
Anthony Fresnel <100720509+AnthoMichel@users.noreply.github.com>
2022-05-18 00:01:14 +0200

    création routing

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+if (empty($_GET['page'])){
+    require_once "view/home.view.php";
+}else {
+    switch($_GET['page']){
+        case "accueil" : require_once "view/home.view.php";
+        break;
+    }
+}
+
+?>
